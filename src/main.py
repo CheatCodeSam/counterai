4865f0ea8cfec0ba925b838bbd5983a061471d96
Carson Weeks <mail@carsonweeks.com>
2024-01-21 13:39:03 -0800

    Added gitignore

--- src/main.py
+++ src/main.py
@@ -1,15 +1,45 @@
-from typing import Union
+from typing import Annotated
+from fastapi import Depends, FastAPI, HTTPException, UploadFile
+import hashlib
+import boto3
+import os
 
-from fastapi import FastAPI
+from mypy_boto3_kms import KMSClient
+
+async def get_kms():
+    client = boto3.client('kms')
+    print("booting")
+    try:
+        yield client
+    finally:
+        print("closing")
+        client.close()
+
+KMSDependency = Annotated[KMSClient, Depends(get_kms)]
 
 app = FastAPI()
 
+@app.post("/generate-certification/")
+async def generate_certification(file: UploadFile):
+    contents = await file.read()
+    print(contents)
+    return
+ 
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/verify/")
+async def verify(signature: str, file: UploadFile, kms: KMSDependency):
+    contents = await file.read()
+    contents.decode()
+    sha256_hash = hashlib.sha256(contents).digest()
+    key_arn = os.environ.get("AWS_KMS_KEY_ARN")
+    if not key_arn:
+        raise HTTPException(status_code=500, detail="Key ARN not in Env")
+    client_res = kms.verify(
+        KeyId=key_arn,
+        Message=sha256_hash,
+        MessageType="RAW",
+        Signature=signature.encode(),
+        SigningAlgorithm="ECDSA_SHA_256"
+        )
+    is_valid_signature = client_res['SignatureValid']
+    return {"filename": file.filename, "sha256": sha256_hash, "is_valid": is_valid_signature}
